be191690492369e7061ac23ebe22d62c5c84b953
sinhvien <sinhvien>
2019-03-29 07:45:27 +0700

    first commit

---
+++ Big_school/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Big_school.MvcApplication" Language="C#" %>
---
+++ Big_school/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Big_school.Startup))]
+namespace Big_school
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
